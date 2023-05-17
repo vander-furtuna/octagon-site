09e039fd76a7ecc11a6ab88dd6fecaa9318b3f88
Vanderlei Furtuna <furtunavanderlei@gmail.com>
2023-05-17 17:02:13 -0300

    feat: add about us section base

---
+++ src/containers/AboutUs/index.tsx
@@ -0,0 +1,12 @@
+import { Title } from '@/components/Title';
+import { AboutUsContainer } from './styles';
+
+export function AboutUs() {
+  return (
+    <AboutUsContainer>
+      <header>
+        <Title>Sobre a Octagon</Title>
+      </header>
+    </AboutUsContainer>
+  );
+}
---
+++ src/containers/AboutUs/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const AboutUsContainer = styled.section`
+  width: 100%;
+  padding: 4rem 2rem;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
