07b7e0e432210b184dd8c8fdb52dcdc627d63639
Vanderlei Furtuna <101757815+vander115@users.noreply.github.com>
2023-08-23 22:41:48 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,13 @@ export default function RootLayout({ children }: { children: ReactNode }) {
                     href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree:wght@400;500;600;700&family=Inter:wght@400;600;700;800&display=swap"
                     rel="stylesheet"
                 />
+                <script type="text/javascript">
+                    (function(c,l,a,r,i,t,y){
+                        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                    })(window, document, "clarity", "script", "ik078yyuhk");
+                </script>
             </head>
             <StyledComponentsRegistry>
                 <ThemeProvider theme={Theme}>
