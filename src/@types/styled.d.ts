7b80b381601cccc816a2c912673f818d8c8c1a48
Vanderlei Furtuna <furtunavanderlei@gmail.com>
2025-07-16 19:37:44 -0300

    remove old data

--- src/@types/styled.d.ts
+++
@@ -1,6 +0,0 @@
-import { ThemeType } from "@/styles/theme";
-import "styled-components";
-
-declare module "styled-components" {
-  export interface DefaultTheme extends ThemeType {}
-}
--- src/dtos/ICommonQuestion.ts
+++
@@ -1,4 +0,0 @@
-export interface ICommonQuestion {
-  question: string;
-  answer: string;
-}
