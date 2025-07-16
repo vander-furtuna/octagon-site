64b1734c6836e2913d4dc4a7ae7b8744cffda84a
Vanderlei Furtuna <furtunavanderlei@gmail.com>
2025-07-16 20:12:29 -0300

    Add new service images and VanderSuncat logo component
    - Added electric-project.jpg, energy-efficiency.jpg, photovoltaic-project.jpg, and technical-report.jpg to the services images directory.
    - Created a new VanderSuncatLogo component in SVG format for branding purposes.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +12,24 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  eslintPluginPrettierRecommended,
+  {
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          singleQuote: true,
+          semi: false,
+          trailingComma: 'all',
+          printWidth: 80,
+          tabWidth: 2,
+          endOfLine: 'auto',
+          arrowParens: 'always',
+        },
+      ],
+      '@next/next/no-img-element': 'off',
+    },
+  },
 ];
 
 export default eslintConfig;
